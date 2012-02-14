658091e1ccb905f6c23da580e6ca040b609bffdb
Ketil Malde <ketil@malde.org>
2012-02-14 15:43:38 +0100

    Add target coverage as well.
    Ignore-this: 5d097d2d444c4e5faff15c6a2cc1aec4
    
    darcs-hash:fef6924940fdbb7fa2c036a80d36e99c8a04d445

--- examples/PslCov.hs
+++ examples/PslCov.hs
@@ -9,10 +9,15 @@ main :: IO ()
 main = do
   args <- getArgs
   case args of 
-    [p,f] -> printPSL . filter (isHit $ read p) =<< readPSL f
-    _ -> error "Usage: pslcov prob pslfile"
+    [pq,pt,f] -> printPSL . filter (isHit (read pq) (read pt)) =<< readPSL f
+    _ -> error ("Usage: pslcov p q pslfile\n"++
+                "where p is the minimum query coverage\n"++
+                "and q is the minium target coverage.")
 
-isHit :: Double -> PSL -> Bool
-isHit p x = fromIntegral (match x) / fromIntegral (qsize x) > p
+isHit :: Double -> Double -> PSL -> Bool
+isHit p q x = m / qs > p && m / ts > q
+  where qs = fromIntegral $ qsize x
+        ts = fromIntegral $ tsize x
+        m  = fromIntegral $ match x
          
 
