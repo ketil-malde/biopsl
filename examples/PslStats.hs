318dbc48b1708de78725296119ff1b620b2dca39
Ketil Malde <ketil@malde.org>
2012-03-27 10:18:03 +0200

    Add PslStats.
    Ignore-this: 4b3b346b9aa7289d99f78a8ba9b9b7c7
    
    darcs-hash:d88b106a69ab0516ef14a68d9d4715c89de5b385

---
+++ examples/PslStats.hs
@@ -0,0 +1,22 @@
+{-| Generate stats from PSL files, similar to 'samtools stats' for
+    BAM files.  Each target sequence is listed with its length and 
+    number of matching query sequences.
+-}
+
+import Bio.Alignment.PSL
+import System.Environment (getArgs)
+import Data.HashMap.Strict as H hiding (map)
+import qualified Data.ByteString.Lazy.Char8 as L
+
+main :: IO ()
+main = do
+  fs <- getArgs
+  ps <- concat `fmap` mapM readPSL fs
+  L.putStrLn $ L.unlines $ map format $ toList $ countTargets ps
+
+countTargets :: [PSL] -> HashMap L.ByteString Int 
+countTargets ps = fromListWith (+) $ map (\p -> (tname p,1)) ps -- todo:tsize
+
+format :: (L.ByteString, Int) -> L.ByteString
+format (tn,p) = L.concat [tn, tab, L.pack (show p)]
+  where tab = L.pack "\t"
