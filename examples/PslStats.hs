b461cf05d451d151edc9f699cc8c33bc48a0de9f
Ketil Malde <ketil@malde.org>
2012-03-27 14:38:49 +0200

    Output lengths, header, and one column per input file.
    Ignore-this: 6a28c4520957098211b43cd5415315df
    (This is similar to asmeval's statcount output, and to samtools
    idxstats for a single file.)
    
    darcs-hash:d2cfcf1c426ee80fe1a43d3936dc285e31b2c745

--- examples/PslStats.hs
+++ examples/PslStats.hs
@@ -7,16 +7,28 @@ import Bio.Alignment.PSL
 import System.Environment (getArgs)
 import Data.HashMap.Strict as H hiding (map)
 import qualified Data.ByteString.Lazy.Char8 as L
+import Data.List (foldl', intercalate)
 
 main :: IO ()
 main = do
   fs <- getArgs
-  ps <- concat `fmap` mapM readPSL fs
-  L.putStr $ L.unlines $ map format $ toList $ countTargets ps
+  ps <- mapM readPSL fs
+  putStrLn ("target\tlenght\t"++intercalate "\t" fs)
+  putStr $ unlines $ format $ countTargets ps
 
-countTargets :: [PSL] -> HashMap L.ByteString Int 
-countTargets ps = fromListWith (+) $ map (\p -> (tname p,1)) ps -- todo:tsize
+-- | map of lengths, and map of count per input file
+countTargets :: [[PSL]] -> [HashMap L.ByteString Int]
+countTargets = go [] empty
+  where go cs ls (ps:pss) = let (l,c) = count1 ls ps
+                            in go (c:cs) l pss
+        go cs ls [] = (ls:reverse cs)
+        count1 ls ps = Data.List.foldl' ins1 (ls,empty) ps
+        ins1 (l,c) p = l `seq` c `seq` (H.insert (tname p) (tsize p) l,H.insertWith (+) (tname p) 1 c)
 
-format :: (L.ByteString, Int) -> L.ByteString
-format (tn,p) = L.concat [tn, tab, L.pack (show p)]
-  where tab = L.pack "\t"
+
+format :: [HashMap L.ByteString Int] -> [String]
+format (l:cs) = [unwords' $ (L.unpack s:show len:map (show . H.lookupDefault 0 s) cs) | (s,len) <- toList l]
+format _ = error "format"
+
+unwords' :: [String] -> String
+unwords' = intercalate "\t"
