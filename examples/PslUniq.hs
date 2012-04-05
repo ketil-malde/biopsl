bb8f77e6b2176f85b09eff717716c2fa96db5865
Ketil Malde <ketil@malde.org>
2012-04-05 14:14:19 +0200

    proteins are not sorted as assumed
    Ignore-this: 6f6d29a870a1d6033aef206511a41240
    
    darcs-hash:f803139b6ca8b47542ae41b6de7eddc0fad36c79

--- examples/PslUniq.hs
+++ examples/PslUniq.hs
@@ -5,6 +5,7 @@
 import Bio.Alignment.PSL
 import System.Environment (getArgs)
 import qualified Data.ByteString.Lazy as L
+import Data.List (sortBy)
 
 main :: IO ()
 main = do
@@ -17,10 +18,16 @@ main = do
   
 uniq :: [PSL] -> [PSL]
 uniq [] = []
-uniq (p:ps) = go p ps
+uniq (p:ps) 
+  -- protein matches are sorted first by strand, thus we need to resort to sorting
+  | L.length (strand p) > 1 = let (q:qs) = sortOn qname (p:ps) in go q qs
+  | otherwise               = go p ps
   
 go :: PSL -> [PSL] -> [PSL]
 go p1 (q:qs) | qname q /= qname p1 = p1 : go q qs
              | match q > match p1  = go q qs
              | otherwise           = go p1 qs
 go p1 [] = [p1]
+
+sortOn :: Ord a1 => (a -> a1) -> [a] -> [a]
+sortOn f = sortBy (\x y -> compare (f x) (f y))
