180afa2232f32f7d6913e90572224bd4a856dbc0
Ketil Malde <ketil@malde.org>
2013-09-19 10:15:22 +0200

    Add -t option to uniquify on target sequence
    Ignore-this: fbf45cf4bb31494a7566ac53172fa86a
    
    darcs-hash:d8345a234f54ea25bf06f690dd6fba5a465a47f4

--- examples/PslUniq.hs
+++ examples/PslUniq.hs
@@ -12,9 +12,10 @@ main = do
   args <- getArgs
   ps <- case args of 
         [] -> (parsePSL `fmap` L.getContents)
+        ["-t",f] -> readPSL f
         [f] -> readPSL f
-        _ -> error "Usage: psluniq [pslfile]"
-  printPSL $ uniq ps
+        _ -> error "Usage: psluniq [-t] [pslfile]"
+  printPSL $ (if null args || head args /= "-t" then uniq else t_uniq) ps 
   
 uniq :: [PSL] -> [PSL]
 uniq [] = []
@@ -22,12 +23,27 @@ uniq (p:ps)
   -- protein matches are sorted first by strand, thus we need to resort to sorting
   | L.length (strand p) > 1 = let (q:qs) = sortOn qname (p:ps) in go q qs
   | otherwise               = go p ps
-  
-go :: PSL -> [PSL] -> [PSL]
-go p1 (q:qs) | qname q /= qname p1 = p1 : go q qs
-             | match q > match p1  = go q qs
-             | otherwise           = go p1 qs
-go p1 [] = [p1]
+ where  
+  go :: PSL -> [PSL] -> [PSL]
+  go p1 (q:qs) | qname q /= qname p1 = p1 : go q qs
+               | match q > match p1  = go q qs
+               | otherwise           = go p1 qs
+  go p1 [] = [p1]
+
+t_uniq :: [PSL] -> [PSL]
+t_uniq [] = []
+t_uniq (p:ps) 
+  -- protein matches are sorted first by strand, thus we need to resort to sorting
+  | L.length (strand p) > 1 = let (q:qs) = sortOn tname (p:ps) in t_go q qs
+  | otherwise               = t_go p ps
+ where
+  t_go :: PSL -> [PSL] -> [PSL]
+  t_go p1 (q:qs) | tname q /= tname p1 = p1 : t_go q qs
+                 | match q > match p1  = t_go q qs
+                 | otherwise           = t_go p1 qs
+  t_go p1 [] = [p1]
+
+
 
 sortOn :: Ord a1 => (a -> a1) -> [a] -> [a]
 sortOn f = sortBy (\x y -> compare (f x) (f y))
